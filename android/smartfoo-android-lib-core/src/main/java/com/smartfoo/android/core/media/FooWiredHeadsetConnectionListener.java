e3d365f02f4314e0758d0702111a545e767f7395
Paul Peavyhouse <pv@swooby.com>
2016-05-24 07:09:40 -0700

    Adding Wired Headset Connection Listener

---
+++ android/smartfoo-android-lib-core/src/main/java/com/smartfoo/android/core/media/FooWiredHeadsetConnectionListener.java
@@ -0,0 +1,207 @@
+package com.smartfoo.android.core.media;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+
+import com.smartfoo.android.core.FooListenerManager;
+import com.smartfoo.android.core.FooString;
+import com.smartfoo.android.core.logging.FooLog;
+
+import java.util.Set;
+
+public class FooWiredHeadsetConnectionListener
+{
+    public interface OnWiredHeadsetConnectedCallbacks
+    {
+        void onWiredHeadsetConnected(String name, boolean hasMicrophone);
+
+        void onWiredHeadsetDisconnected(String name, boolean hasMicrophone);
+    }
+
+    private final WiredHeadsetBroadcastReceiver                        mWiredHeadsetBroadcastReceiver;
+    private final FooListenerManager<OnWiredHeadsetConnectedCallbacks> mListenerManager;
+
+    public FooWiredHeadsetConnectionListener(
+            @NonNull
+            Context applicationContext)
+    {
+        mWiredHeadsetBroadcastReceiver = new WiredHeadsetBroadcastReceiver(applicationContext);
+        mListenerManager = new FooListenerManager<>();
+    }
+
+    public boolean isStarted()
+    {
+        return mWiredHeadsetBroadcastReceiver.isStarted();
+    }
+
+    public boolean isWiredHeadsetInserted()
+    {
+        return mWiredHeadsetBroadcastReceiver.isWiredHeadsetInserted();
+    }
+
+    public void attach(
+            @NonNull
+            OnWiredHeadsetConnectedCallbacks callbacks)
+    {
+        mListenerManager.attach(callbacks);
+
+        if (isStarted())
+        {
+            return;
+        }
+
+        mWiredHeadsetBroadcastReceiver.start(new OnWiredHeadsetConnectedCallbacks()
+        {
+            @Override
+            public void onWiredHeadsetConnected(String name, boolean hasMicrophone)
+            {
+                Set<OnWiredHeadsetConnectedCallbacks> callbacks = mListenerManager.beginTraversing();
+                for (OnWiredHeadsetConnectedCallbacks callback : callbacks)
+                {
+                    callback.onWiredHeadsetConnected(name, hasMicrophone);
+                }
+                mListenerManager.endTraversing();
+            }
+
+            @Override
+            public void onWiredHeadsetDisconnected(String name, boolean hasMicrophone)
+            {
+                Set<OnWiredHeadsetConnectedCallbacks> callbacks = mListenerManager.beginTraversing();
+                for (OnWiredHeadsetConnectedCallbacks callback : callbacks)
+                {
+                    callback.onWiredHeadsetDisconnected(name, hasMicrophone);
+                }
+                mListenerManager.endTraversing();
+            }
+        });
+    }
+
+    public void detach(
+            @NonNull
+            OnWiredHeadsetConnectedCallbacks callbacks)
+    {
+        mListenerManager.detach(callbacks);
+
+        if (mListenerManager.isEmpty())
+        {
+            mWiredHeadsetBroadcastReceiver.stop();
+        }
+    }
+
+    private static class WiredHeadsetBroadcastReceiver
+            extends BroadcastReceiver
+    {
+        private static final String TAG = FooLog.TAG(WiredHeadsetBroadcastReceiver.class);
+
+        private final Context mApplicationContext;
+
+        private final Object mSyncLock = new Object();
+
+        private boolean mIsStarted;
+
+        private OnWiredHeadsetConnectedCallbacks mCallbacks;
+
+        private boolean mIsWiredHeadsetInserted;
+
+        public WiredHeadsetBroadcastReceiver(Context applicationContext)
+        {
+            mApplicationContext = applicationContext;
+        }
+
+        public boolean isWiredHeadsetInserted()
+        {
+            return mIsWiredHeadsetInserted;
+        }
+
+        public boolean isStarted()
+        {
+            synchronized (mSyncLock)
+            {
+                return mIsStarted;
+            }
+        }
+
+        public void start(
+                @NonNull
+                OnWiredHeadsetConnectedCallbacks callbacks)
+        {
+            FooLog.v(TAG, "+start(...)");
+            synchronized (mSyncLock)
+            {
+                if (!mIsStarted)
+                {
+                    mIsStarted = true;
+
+                    mCallbacks = callbacks;
+
+                    IntentFilter intentFilter = new IntentFilter();
+                    if (Build.VERSION.SDK_INT >= 21)
+                    {
+                        intentFilter.addAction(AudioManager.ACTION_HEADSET_PLUG);
+                    }
+                    else
+                    {
+                        intentFilter.addAction(Intent.ACTION_HEADSET_PLUG);
+                    }
+                    mApplicationContext.registerReceiver(this, intentFilter);
+                }
+            }
+            FooLog.v(TAG, "-start(...)");
+        }
+
+        public void stop()
+        {
+            FooLog.v(TAG, "+stop()");
+            synchronized (mSyncLock)
+            {
+                if (mIsStarted)
+                {
+                    mIsStarted = false;
+
+                    mApplicationContext.unregisterReceiver(this);
+                }
+            }
+            FooLog.v(TAG, "-stop()");
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            String action = intent.getAction();
+            FooLog.v(TAG, "onReceive: action=" + FooString.quote(action));
+
+            switch (action)
+            {
+                case AudioManager.ACTION_HEADSET_PLUG:
+                {
+                    int state = intent.getIntExtra("state", 0); // 0 for unplugged, 1 for plugged
+                    String name = intent.getStringExtra("name"); // Headset type, human readable string
+                    int microphone = intent.getIntExtra("microphone", 0); // 1 if headset has a microphone, 0 otherwise
+                    FooLog.v(TAG, "onReceive: state=" + state);
+                    FooLog.v(TAG, "onReceive: name=" + FooString.quote(name));
+                    FooLog.v(TAG, "onReceive: microphone=" + microphone);
+
+                    synchronized (mSyncLock)
+                    {
+                        mIsWiredHeadsetInserted = state == 1;
+                        if (mIsWiredHeadsetInserted)
+                        {
+                            mCallbacks.onWiredHeadsetConnected(name, microphone == 1);
+                        }
+                        else
+                        {
+                            mCallbacks.onWiredHeadsetDisconnected(name, microphone == 1);
+                        }
+                    }
+
+                    break;
+                }
+            }
+        }
+    }
+}
