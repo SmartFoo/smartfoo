84ab099481cf4ea225053ca0cabde4707f2579ca
Paul Peavyhouse <pv@swooby.com>
2018-05-03 12:27:49 -0700

    Hopefully fixing build break

--- android/smartfoo-android-testapp/src/main/java/com/smartfoo/android/testapp/MainActivity.kt
+++ android/smartfoo-android-testapp/src/main/java/com/smartfoo/android/testapp/MainActivity.kt
@@ -6,12 +6,18 @@ import android.support.design.widget.Snackbar
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    companion object {
+        val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,6 +34,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+
+        buttonFoo.setOnClickListener {
+        }
     }
 
     override fun onBackPressed() {
