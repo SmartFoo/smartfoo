be6be54acb4cbd2d3be1b8b7cce0422ea8b439f1
Paul Peavyhouse <pv@swooby.com>
2025-03-12 10:37:08 -0700

    FooTextToSpeech: Default speed to 1.35; tiny log cleanup

--- android/smartfoo-android-lib-core/src/main/java/com/smartfoo/android/core/texttospeech/FooTextToSpeech.kt
+++ android/smartfoo-android-lib-core/src/main/java/com/smartfoo/android/core/texttospeech/FooTextToSpeech.kt
@@ -34,7 +34,7 @@ class FooTextToSpeech private constructor() {
         var VERBOSE_LOG_UTTERANCE_PROGRESS = false
         var VERBOSE_LOG_AUDIO_FOCUS = false
 
-        const val DEFAULT_VOICE_SPEED = 2.0f
+        const val DEFAULT_VOICE_SPEED = 1.35f
         const val DEFAULT_VOICE_PITCH = 1.0f
         const val DEFAULT_VOICE_VOLUME = 1.0f
 
@@ -367,10 +367,10 @@ class FooTextToSpeech private constructor() {
                 FooLog.v(TAG, "runAfterSpeak: mUtteranceCallbacks.size() == 0; audioFocusStop()")
                 mAudioFocusListener.audioFocusStop(mAudioFocusListenerCallbacks)
             } else {
-                FooLog.v(TAG, "runAfterSpeak: mUtteranceCallbacks.size()($size) > 0; ignoring")
+                FooLog.v(TAG, "runAfterSpeak: mUtteranceCallbacks.size()($size) > 0; ignoring (not calling `audioFocusStop()`)")
             }
         }
-        FooLog.v(TAG, "+runAfterSpeak()")
+        FooLog.v(TAG, "-runAfterSpeak()")
     }
 
     private fun onError(utteranceId: String, errorCode: Int) {
